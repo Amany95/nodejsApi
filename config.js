297562356ed93eb59faca7e4747f72237f0a2e3c
amany adel <36702658+Amany95@users.noreply.github.com>
2019-12-30 14:17:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
 
     /* Database Credentials */
-    __MYSQL_DB_HOST__: "localhost",
-    __MYSQL_DB_NAME__: "task",
-    __MYSQL_DB_USER__: "root",
-    __MYSQL_DB_PASS__: "",
+  //  __MYSQL_DB_HOST__: "localhost",
+   // __MYSQL_DB_NAME__: "task",
+   // __MYSQL_DB_USER__: "root",
+   // __MYSQL_DB_PASS__: "",
      /* Database Credentials */
-    //  __MYSQL_DB_HOST__: "127.0.0.1",
-    //  __MYSQL_DB_NAME__: "expocart_expo_cartoon_website",
-    //  __MYSQL_DB_USER__: "expocart_website",
-    //  __MYSQL_DB_PASS__: "&z]]hT2VX!Yu",
+      __MYSQL_DB_HOST__: "127.0.0.1",
+      __MYSQL_DB_NAME__: "expocart_expo_cartoon_website",
+      __MYSQL_DB_USER__: "expocart_website",
+      __MYSQL_DB_PASS__: "&z]]hT2VX!Yu",
 
 	/* Memcached Credentials */
     // __MEMCACHED_HOST__: '127.0.0.1:3306',
