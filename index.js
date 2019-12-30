1d416d14084fc38d080f7a45c8b6a3485d5a300e
amany adel <36702658+Amany95@users.noreply.github.com>
2019-12-30 15:22:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.use(cors())
 app.use(userRouter);
 app.use('/task', taskRouter);
 
+app.get('/',(req,res)=>{
+    res.send('hello');
+})
+
 const port=process.env.PORT || 3001;
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
